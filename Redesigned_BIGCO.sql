393abe54c1a80b8b76e261421d46c390d79523b1
andro095 <andre.rodriguezgt@yahoo.com>
2024-11-13 01:57:35 -0500

    Redesigned_BIGCO.sql

---
+++ Redesigned_BIGCO.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA HR AUTHORIZATION dbo;
+GO
+CREATE SCHEMA Production AUTHORIZATION dbo;
+GO
+CREATE SCHEMA Sales AUTHORIZATION dbo;
+GO
+CREATE SCHEMA Info AUTHORIZATION dbo;
+GO
+
+---------------------------------------------------------------------
+-- Create Tables
+---------------------------------------------------------------------
+
+-- Production Tables Creation
